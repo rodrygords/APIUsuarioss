09de25c1a16c98a25013010ecabc57d8715cc500
rodrygords <rodrygords@gmail.com>
2025-12-01 12:41:39 -0300

    feat(Domain/Entities): Adiciona entidade Usuario

---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+namespace APIUsuarios.Domain.Entities;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Senha { get; set; } = string.Empty;
+    public DateTime DataNascimento { get; set; }
+    public string? Telefone { get; set; }
+    public bool Ativo { get; set; } = true;
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    public DateTime? DataAtualizacao { get; set; }
+}
